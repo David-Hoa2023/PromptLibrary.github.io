3b0d7706f5a1b95e8a73aef60e968676f767f67b
David Hoang <125635717+David-Hoa2023@users.noreply.github.com>
2024-07-26 19:15:06 +0700

    Update getData.js

--- netlify/functions/getData.js
+++ netlify/functions/getData.js
@@ -4,12 +4,15 @@ const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 exports.handler = async function(event, context) {
+  console.log('getData function called');
   try {
     await client.connect();
+    console.log('Connected to MongoDB');
     const database = client.db('promptLibrary');
     const collection = database.collection('data');
 
     const data = await collection.find({}).toArray();
+    console.log('Data fetched:', data);
     const result = data.reduce((acc, item) => {
       acc[item.key] = item.value;
       return acc;
@@ -20,7 +23,8 @@ exports.handler = async function(event, context) {
       body: JSON.stringify(result),
     };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() };
+    console.error('Error in getData:', error);
+    return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
   } finally {
     await client.close();
   }
