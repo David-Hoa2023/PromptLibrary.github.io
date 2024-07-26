424b13b0d4216161ff7cd8cc6f375798280e775d
David Hoang <125635717+David-Hoa2023@users.noreply.github.com>
2024-07-26 20:05:25 +0700

    Update saveData.js

--- netlify/functions/saveData.js
+++ netlify/functions/saveData.js
@@ -37,6 +37,6 @@ exports.handler = async function(event, context) {
       body: JSON.stringify({ error: error.message, stack: error.stack }) 
     };
   } finally {
-    if (connection) await connection.close();
+    if (connection) await client.close();
   }
 };
